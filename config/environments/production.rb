71dff1d52239c40a81eed0b489987c1c9ecce33c
Thomas Deschamps <thomasdscmps@gmail.com>
2014-03-26 13:53:52 +0100

    Added jQuery Masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,13 @@ Pinteresting::Application.configure do
   #Note to set this to actual host
   config.action_mailer.default_url_options = { host: 'rahanfilsdecrahan' }
 
+    config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
